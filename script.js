686c71d8d5993d31069e1292ec6840c2df0eeba0
Abdirahman Mohamed (Buushe) <79105703+Buushe@users.noreply.github.com>
2022-11-06 21:13:47 +0600

    js-exercise-if-II

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello");
+let grade = 100
+//  grade = 80
+//  grade = 60
+//  grade = 50
+
+if(grade >= 100 && grade >= 90){
+    console.log("You got an A grade")
+}else if(grade <= 90 && grade >= 75){
+    console.log("You got an B grade")
+}else if(grade <= 75 && grade >= 60){
+    console.log("You got an C grade")
+} 
+else if (grade < 60)
+{
+    console.log("You an FAILED")
+}
+
