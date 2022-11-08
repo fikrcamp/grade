4c6b9f6954b6b29b16744353b39467057d6ed13f
hamda <hamda.baashe44@gmail.com>
2022-11-08 18:57:03 +0000

    I finished Js exercise-2

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello");
+
+
+let grade = 0;
+if (grade >= 90 && grade <= 100){
+    console.log("You got an A grade");
+}
+else if (grade >= 75 && grade < 90){
+    console.log("You got a B grade");
+}
+else if (grade >= 60 && grade < 75){
+    console.log("You got a C grade");
+}
+else if (grade < 60) {
+    console.log("You Failed");
+}
