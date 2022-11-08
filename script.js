13768484354f4b44e45a3a1175612737fa218535
Hamda Mouhoumed <75206869+xamdoo@users.noreply.github.com>
2022-11-08 22:25:25 +0300

    Solved

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello");
+let grade = 77
+
+if(grade >= 90 && grade <= 100){
+    console.log("You got an A grade")
+}else if(grade >= 75 && grade < 90){
+    console.log("You got an B grade")
+}else if(grade >= 60 && grade < 75){
+    console.log("You got an C grade")
+}else if(grade < 60){
+    console.log("You got an FAILED")
+}
