11d851a554a8bd12f6a9f8f712bd51cf83622636
AbduleYousuf <abduleyousuf08@gmail.com>
2022-11-08 15:13:14 +0300

    exercise || solve

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("hello");
+
+
+let grade = 90;
+
+if (grade >= 90 && grade <= 100) {
+    console.log("you got an A")
+} else if (grade >= 75 && grade <= 90) {
+    console.log("you got an b")
+} else if (grade >= 60 && grade <= 75) {
+    console.log("you got an c")
+} else {
+    console.log("you got an failed")
+}
