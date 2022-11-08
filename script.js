699e1b0feef721bd5cca3998c19cd24fbb703cf0
mohamed-hassanjj <mohassanosman@gmail.com>
2022-11-08 09:12:29 +0300

    'if' exercise two

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 console.log("hello");
 
-const grade = 68
+const grade = 100
 
-if(grade < 100 && grade > 90){
+if(grade <= 100 && grade >= 90){
     console.log("You got an A grade")
-}else if(grade < 90 && grade > 75){
+}else if(grade <= 90 && grade >= 75){
     console.log("You got an B grade")
-}else if(grade < 75 && grade > 60){
+}else if(grade <= 75 && grade >= 60){
     console.log("You got an C grade")
-}else if(grade < 60){
+}else if(grade <= 60){
     console.log("You got an FAILED")
 }else{
     console("Good Luck")
