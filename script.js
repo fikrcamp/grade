aa3159c09465d3378a8631e2ecf1f4279d84c372
mustafe abdirahman <111310904+carabzl@users.noreply.github.com>
2022-11-09 14:35:29 +0300

    js exerxise-2

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("hello");
+let grade = 14
+ 
+if(grade <= 100 && grade >= 90){
+   console.log("you Got A!")
+}else if(grade <= 89 && grade >= 75){
+   console.log("you Got B!")
+}else if(grade <=74 && grade >= 60){
+   console.log("you Got C!")
+}else if(grade <= 59 && grade >= 0){
+   console.log("you FAILED!")
+}
+else{
+   console.log("out of range")
+} 
+
+
