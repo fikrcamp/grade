152ff0e699652fa873dd0d7a20c3992327bef172
Mar01yam <ashqaraar11@gmail.com>
2022-11-09 19:02:32 +0300

    finished 2nd js exercise

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("hello");
+let waris= 90
+console.log(waris)
+
+const nuux=90
+if(nuux>80){
+    console.log("nuux got an A grade")
+}
+ hodan=80
+ if(hodan>70){
+    console.log("hodan got a B grade")
+ }
+
+ faarax=65
+ if (faarax>50){
+    console.log("faarax got a C grade")
+ }
+
+farxiya=40
+if(farxiya<50){
+    console.log("farxiya got FAILED")
+}
