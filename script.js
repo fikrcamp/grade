95b7c586df5dfc1dd299d938535183ecaffde826
Asma mohamed <67588350+asma-ux@users.noreply.github.com>
2022-11-09 08:25:35 -0800

    Answered the js second exercise

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello");
+let grade = 78;
+
+if (grade <= 100 && grade >= 90) {
+  console.log("you got an A grade");
+} else if (grade < 90 && grade >= 75) {
+  console.log("You got a B grade");
+} else if (grade < 75 && grade >= 60) {
+  console.log("you got a c grade");
+} else if (grade < 60) {
+  console.log("You got FAILED");
+}
